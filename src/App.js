7ba132f5e82663ae5449bda45685696cc761a6c0
mss38 <mss38k@gmail.com>
2022-06-30 19:26:26 +0600

    tailwind basic setup on react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <h1>Project Start Now</h1>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      {
+        dark: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
